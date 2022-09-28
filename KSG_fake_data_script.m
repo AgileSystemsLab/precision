98b35c628e4ddf3e320f32798634db6b8ac0a3e2
LeoJW <leo@woodtx.com>
2022-09-28 12:48:15 -0400

    fully synthetic dataset section improved, now runs over different correlation levels

--- KSG_fake_data_script.m
+++ KSG_fake_data_script.m
@@ -130,157 +130,68 @@ ylabel('Measured precision (ms)')
 
 %% Fully synthetic dataset from bivariate gaussian
 
-synth_repeats = 50;
+repeats_at_corr = 1;
 num_points = 2500;
+corr = [0.5, 0.6, 0.7, 0.8, 0.9];
+ncorr = length(corr);
 
 % Generate fully synthetic dataset 
 muX = 0;         %X mean of the bivariate gaussian
 muY = 0;         %Y mean of the bivariate gaussian
 sigmaX = 2;      %X standard deviation   
 sigmaY = 2;      %Y standard deviation
-correlation = 0.9;
 
 % preallocate
-synthX_disc = cell(synth_repeats,n);
-synthY = cell(synth_repeats, 1);
-MI_synth = cell(synth_repeats,n);
-synthX_disc(:) = {nan(num_points, 1)};
-synthY(:) = {nan(num_points, 1)};
+MI_synth = cell(ncorr, repeats_at_corr, n);
 MI_synth(:) = {zeros(length(noise), repeats)};
-for r = 1:synth_repeats
-    %generate a correlated X and Y
-    x1 = normrnd(0, 1, num_points, 1);
-    x2 = normrnd(0, 1, num_points, 1);
-    x3 = correlation .* x1 + (1 - correlation^2)^.5 .* x2;
-    synthX = muX + x1 * sigmaX;
-    synthY{r} = muY + x3 * sigmaY;
-    % Loop over fixed precision levels
-    for i = 1:n
-        synthX_disc{r,i} = round(synthX / prec_levels(i)) * prec_levels(i);
-        MI_synth{r,i} = KSG_precision(synthX_disc{r,i}, synthY{r}, knn, repeats, noise);
+for i = 1:ncorr
+    for r = 1:repeats_at_corr
+        %generate a correlated X and Y
+        x1 = normrnd(0, 1, num_points, 1);
+        x2 = normrnd(0, 1, num_points, 1);
+        x3 = corr(i) .* x1 + (1 - corr(i)^2)^.5 .* x2;
+        synthX = muX + x1 * sigmaX;
+        synthY = muY + x3 * sigmaY;
+        % Loop over fixed precision levels
+        for k = 1:n
+            synthX_disc = round(synthX / prec_levels(k)) * prec_levels(k);
+            MI_synth{i,j,k} = KSG_precision(synthX_disc, synthY, knn, repeats, noise);
+        end
     end
 end
 
 % Save results
-save('KSG_sim_synthetic_data_fixed_precision.mat', 'MI_synth', 'synthX_disc', 'synthY', ...
-    'noise', 'repeats', 'knn', 'synth_repeats', 'num_points', 'correlation')
+save('KSG_sim_synthetic_data_fixed_precision.mat', 'MI_synth', ...
+    'noise', 'repeats', 'knn', 'corr', 'repeats_at_corr', 'num_points', 'correlation')
 
-%%
 
-i = 3;
-
-figure
-hold on
-cols = copper(n);
-for j = 1:n
-    plot(log10(noise), mean(MI_synth{i,j}, 2), 'color', cols(j,:))
-end
-
-%%
-% % Plot MI vs noise curves
-% figure
-% hold on
-% cols = copper(n);
-% for i = 1:n
-%     plot(log10(noise), mean(MI_synth{i}, 2), 'color', cols(i,:));
-% end
-% MI_synth_real = KSG_precision(synthX, synthY, knn, repeats, noise);
-% mseb(log10(noise), mean(MI_synth_real,2), std(MI_synth_real, 0, 2)');
-% xlabel('log10(noise)')
-% ylabel('MI (bits)')
+deriv_thresh_scale = 0.3;
 
 % Plot a priori precision vs actual
-synth_precision = zeros(synth_repeats, n);
-synth_deriv_precision = zeros(synth_repeats, n);
-for r = 1:synth_repeats
-    for i = 1:n
-        meanMI = mean(MI_synth{r,i}, 2);
-        % Original method
-        mis = MI_KSG_subsampling_multispike(synthX_disc{r,i}, synthY{r}, knn, (1:4));
-        mi_sd = findMI_KSG_stddev(mis, num_points, false);
-        ind = find(meanMI < (MI_synth{r,i}(1,1) - mi_sd), 1);
-        synth_precision(r,i) = noise(ind);
-        % Derivative method
-        pad = [nan(1, sg_window), meanMI', nan(1, sg_window)];
-        grad = conv(pad, -1 * g(:,2), 'same'); 
-        grad = grad(sg_window+1:end-sg_window);
-        deriv_thresh = min(grad) * deriv_thresh_scale;
-        ind = find(grad < deriv_thresh, 1);
-        synth_deriv_precision(r,i) = noise(ind);
+% Find actual precision
+synth_precision = zeros(ncorr, repeats_at_corr, n);
+for i = 1:ncorr
+    for j = 1:repeats_at_corr
+        for k = 1:n
+            pad = [nan(1, sg_window), mean(MI_synth{i,j,k}, 2)', nan(1, sg_window)];
+            grad = conv(pad, -1 * g(:,2), 'same'); 
+            grad = grad(sg_window+1:end-sg_window);
+            deriv_thresh = min(grad) * deriv_thresh_scale;
+            ind = find(grad < deriv_thresh, 1);
+            synth_precision(i,j,k) = noise(ind);
+        end
     end
 end
+synth_precision = reshape(synth_precision, [ncorr*repeats_at_corr, n]);
 
 figure
 hold on
-for i = 1:synth_repeats
-    plot(prec_levels, synth_precision(i,:), '*')
+for i = 1:ncorr*repeats_at_corr
+        plot(prec_levels, synth_precision(i,:), '*')
 end
 plot(get(gca, 'xlim'), get(gca,'xlim'), 'k-')
-title('original')
 xlabel('A priori precision')
 ylabel('Observed precision')
-figure
-hold on
-for i = 1:synth_repeats
-    plot(prec_levels, synth_deriv_precision(i,:), '*')
-end
-plot(get(gca, 'xlim'), get(gca,'xlim'), 'k-')
-title('derivative')
-xlabel('A priori precision')
-ylabel('Observed precision')
-
-
-% %% Fully synthetic dataset, vary correlation factor
-% corr = [0.4, 0.6, 0.8, 0.9];
-% 
-% num_points = 2700;
-% muX = 0;         %X mean of the bivariate gaussian
-% muY = 0;         %Y mean of the bivariate gaussian
-% sigmaX = 2;      %X standard deviation   
-% sigmaY = 2;      %Y standard deviation
-% %generate a correlated X and Y
-% x1 = normrnd(0, 1, num_points, 1);
-% x2 = normrnd(0, 1, num_points, 1);
-% 
-% figure(1)
-% hold on
-% figure(2)
-% hold on
-% cols = copper(length(corr));
-% 
-% 
-% 
-% for j = 1:length(corr)
-%     correlation = corr(i);
-%     x3 = correlation .* x1 + (1 - correlation^2)^.5 .* x2;
-%     synthX = muX + x1 * sigmaX;
-%     synthY = muY + x3 * sigmaY;
-%     MI = KSG_precision(synthX, synthY, knn, repeats, noise);
-%     figure(1)
-%     mseb(log10(noise), mean(MI, 2), std(MI, 0, 2)', struct('col', {{cols(i,:)}}));
-%     synthX_disc = cell(1,n);
-%     MI_synth = cell(1,n);
-%     bin_edges = cell(1,n);
-%     bin = cell(1,n);
-%     synthX_disc(:) = {nan(size(synthX))};
-%     MI_synth(:) = {zeros(length(noise), repeats)};
-%     % Loop over bins to discretize by
-%     for i = 1:n
-%         [~, bin_edges{i}, bin{i}] = histcounts(synthX, nbin(i));
-%         synthX_disc{i} = bin_edges{i}(bin{i} + 1);
-%         synthX_disc{i}(synthX_disc{i}==bin_edges{i}(1)) = nan;
-%         % Indexing above can sometimes cause transpose, so undo if it happens
-%         if size(synthX_disc{i}, 1) < size(synthX_disc{i}, 2) 
-%             synthX_disc{i} = synthX_disc{i}';
-%         end
-%         MI_synth{i} = KSG_precision(synthX_disc{i}, synthY, knn, repeats, noise);
-%     end
-% 
-%     figure(2)
-% 
-% 
-% end
-
 
 
 
