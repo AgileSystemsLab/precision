0f357a7dd76b4be181fcb504f3aa627fda9912c2
LeoJW <leo@woodtx.com>
2022-10-11 22:25:30 -0400

    run knn distances on all finished

--- KNN_distance_script.m
+++ KNN_distance_script.m
@@ -57,12 +57,8 @@ for jj = unq(unq~=0)'
                 disty = max(sqrt((noiseY(i,:) - noiseY(idy(knn),:)).^2));
                 % Select max distance, put back in regular units
                 if distx > disty
-%                     kdist{ii,jj}(i) = sum((((noiseX(idx(knn),:)/xsd+xme) - (noiseX(i,:)/xsd+xme))).^2).^0.5;
-%                     kdist{ii,jj}(i) = sum((noiseX(idx(knn),:) - noiseX(i,:)).^2).^0.5;
                     kdist{ii,jj}(i) = distx;
                 else
-%                     kdist{ii,jj}(i) = sum((((noiseY(idy(knn),:)/ysd+yme) - (noiseY(i,:)/ysd+yme))).^2).^0.5;
-%                     kdist{ii,jj}(i) = sum((noiseY(idy(knn),:) - noiseY(i,:)).^2).^0.5;
                     kdist{ii,jj}(i) = disty;
                 end
             end
@@ -106,55 +102,80 @@ probs(unq==0) = [];
 scale = sum(probs' .* medians(1,:), 'omitnan') / sum(probs,'omitnan')
 
 toc
-% %% Run ALL
-% 
-% %---- Load data
-% load(fullfile('Data',['Moth',num2str(moth),'_MIdata.mat']))
-% % Rename so it's easier to write, get some useful quantities out
-% X = time_data.([muscle,'strokes']);
-% Y = Tz_WSd;
-% Nspike = sum(~isnan(X), 2);
-% unq = unique(Nspike);
-% 
-% % Levels of noise to check
-% noiselevels = [0.75, 1.5, 3, 6];
-% ncheck = length(noiselevels);
-% kdist = cell(ncheck, length(unq(unq~=0)));
-% % Loop over number of spikes in a wingbeat
-% for jj = unq(unq~=0)'
-%     useX = X(Nspike==jj, 1:jj);
-%     useY = Y(Nspike==jj, :);
-%     % Continue only if enough wingbeats with this many spikes
-%     if size(useX, 1) >= knn
-%         % Loop over noise levels
-%         for ii = 1:ncheck
-%             kdist{ii,jj} = nan(size(useX,1),1);
-%             noiseX = useX + noiselevels(ii) * rand(size(useX));
-%             noiseY = useY;
-%             % Z-score X and Y by column (same as in kraskov C code)
-%             xme = mean(noiseX, 1); 
-%             xsd = std(noiseX, 1);
-%             yme = mean(noiseY, 1); 
-%             ysd = std(noiseY, 1);
-%             noiseX = (noiseX - xme) .* xsd;
-%             noiseY = (noiseY - yme) .* ysd;
-%             % Loop over each point in X, get knn distances
-%             % (Can improve by passing all points to knnsearch in single call)
-%             for i = 1:size(useX,1)
-%                 % Get indices of kth nearest neighbors, correct indices (X and Y)
-%                 [idx, distx] = knnsearch(noiseX([1:(i-1), (i+1):end], :), noiseX(i,:), 'K', knn);
-%                 idx(idx>i) = idx(idx>i) + 1;
-%                 [idy, disty] = knnsearch(useY([1:(i-1), (i+1):end], :), useY(i,:), 'K', knn);
-%                 idy(idy>i) = idy(idy>i) + 1;
-%                 % Select max distance, put back in regular units
-%                 if distx(knn) > disty(knn)
-% %                     kdist{ii,jj}(i) = sum((((noiseX(idx(knn),:)/xsd+xme) - (noiseX(i,:)/xsd+xme))).^2).^0.5;
-%                     kdist{ii,jj}(i) = sum((noiseX(idx(knn),:) - noiseX(i,:)).^2).^0.5;
-%                 else
-% %                     kdist{ii,jj}(i) = sum((((noiseY(idy(knn),:)/ysd+yme) - (noiseY(i,:)/ysd+yme))).^2).^0.5;
-%                     kdist{ii,jj}(i) = sum((noiseY(idy(knn),:) - noiseY(i,:)).^2).^0.5;
-%                 end
-%             end
-%         end
-%     end
-% end
+
+%% Run ALL
+nmoths = 7;
+nmuscles = 10;
+
+% Levels of noise to check
+noiselevels = [0.75, 1.5, 3, 6];
+ncheck = length(noiselevels);
+
+% Preallocate
+kdist = cell(nmoths, nmuscles);
+xme = cell(nmoths, nmuscles);
+yme = cell(nmoths, nmuscles);
+xsd = cell(nmoths, nmuscles);
+ysd = cell(nmoths, nmuscles);
+
+for mothi = 1:nmoths
+    %---- Load data
+    load(fullfile('Data',['Moth',num2str(mothi),'_MIdata.mat']))
+    fields = fieldnames(time_data);
+    disp(['Moth ', num2str(mothi)])
+    for musci = 1:nmuscles
+        disp(fields{musci}(1:end-7))
+        % Rename so it's easier to write, get some useful quantities out
+        X = time_data.(fields{musci});
+        Y = Tz_WSd;
+        Nspike = sum(~isnan(X), 2);
+        unq = unique(Nspike);
+        % Preallocate
+        kdist{mothi,musci} = cell(ncheck, length(unq(unq~=0)));
+        xme{mothi,musci} = cell(ncheck, length(unq(unq~=0)));
+        yme{mothi,musci} = cell(ncheck, length(unq(unq~=0)));
+        xsd{mothi,musci} = cell(ncheck, length(unq(unq~=0)));
+        ysd{mothi,musci} = cell(ncheck, length(unq(unq~=0)));
+        % Loop over number of spikes in a wingbeat
+        for jj = unq(unq~=0)'
+            useX = X(Nspike==jj, 1:jj);
+            useY = Y(Nspike==jj, :);
+            % Continue only if enough wingbeats with this many spikes
+            if size(useX, 1) >= knn
+                % Loop over noise levels
+                for ii = 1:ncheck
+                    kdist{mothi,musci}{ii,jj} = nan(size(useX,1),1);
+                    noiseX = useX + noiselevels(ii) * rand(size(useX));
+                    noiseY = useY;
+                    % Z-score X and Y by column (same as in kraskov C code)
+                    xme{mothi,musci}{ii,jj} = mean(noiseX, 1); 
+                    xsd{mothi,musci}{ii,jj} = std(noiseX, 1);
+                    yme{mothi,musci}{ii,jj} = mean(noiseY, 1); 
+                    ysd{mothi,musci}{ii,jj} = std(noiseY, 1);
+                    noiseX = (noiseX - xme{mothi,musci}{ii,jj}) ./ xsd{mothi,musci}{ii,jj};
+                    noiseY = (noiseY - yme{mothi,musci}{ii,jj}) ./ ysd{mothi,musci}{ii,jj};
+                    % Loop over each point in X, get knn distances
+                    % (Can improve by passing all points to knnsearch in single call)
+                    for i = 1:size(useX,1)
+                        % Get indices of kth nearest neighbors, correct indices (X and Y)
+                        [idx, ~] = knnsearch(noiseX([1:(i-1), (i+1):end], :), noiseX(i,:), 'K', knn, 'NSMethod', 'kdtree');
+                        idx(idx>i) = idx(idx>i) + 1;
+                        [idy, ~] = knnsearch(useY([1:(i-1), (i+1):end], :), useY(i,:), 'K', knn, 'NSMethod', 'kdtree');
+                        idy(idy>i) = idy(idy>i) + 1;
+                        % Calculate max norm distances
+                        distx = max(sqrt((noiseX(i,:) - noiseX(idx(knn),:)).^2));
+                        disty = max(sqrt((noiseY(i,:) - noiseY(idy(knn),:)).^2));
+                        % Select max distance, put back in regular units
+                        if distx > disty
+                            kdist{mothi,musci}{ii,jj}(i) = distx;
+                        else
+                            kdist{mothi,musci}{ii,jj}(i) = disty;
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+save('KSG_data_knn_distances.mat', 'kdist', 'xme', 'yme', 'xsd', 'ysd', 'noiselevels', 'ncheck');
